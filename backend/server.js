75401a8cb88a03db8dc17e46d3c127cdbc0981a7
DigitalBotanist <oshada1188@gmail.com>
2025-03-18 16:04:32 +0530

    added swagger

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,13 @@ import express from 'express'
 import mongoose from 'mongoose';
 import http from 'http'
 import cors from 'cors'
+import { serve, setup } from 'swagger-ui-express';
 import FleetManager from './FleetManagement/FleetManager.js';
+import YAML from 'yamljs';
+import path from 'path';
+import { readFileSync, readdirSync } from 'fs';
+import { fileURLToPath } from "url";
+
 
 // routes 
 import fleetRoutes from './FleetManagement/routes/fleet.js'
@@ -20,11 +26,50 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Load Swagger YAML files
+const swaggerDirectory = path.join(__dirname, 'swagger');
+const swaggerFiles = readdirSync(swaggerDirectory);
+
+
+
+const swaggerDocs = swaggerFiles.map((file) => {
+    const filePath = path.join(swaggerDirectory, file);
+    const fileContent = readFileSync(filePath, 'utf8');
+    return YAML.parse(fileContent);
+  });
+
+  const combinedSwaggerSpec = {
+    openapi: '3.0.0',
+    info: {
+      title: 'MERN API',
+      version: '1.0.0',
+      description: 'API documentation for the MERN project',
+    },
+    paths: {},
+    components: {
+        securitySchemes: {
+          BearerAuth: {
+            type: 'http',
+            scheme: 'bearer',
+            bearerFormat: 'JWT',
+          },
+        },
+      },
+  };
+
+  swaggerDocs.forEach((doc) => {
+    if (doc.paths) Object.assign(combinedSwaggerSpec.paths, doc.paths);
+    if (doc.components) Object.assign(combinedSwaggerSpec.components, doc.components);
+  });
+
 // fleet socket server 
 const fleetServer = http.createServer(app)
 const fleetManager = new FleetManager(fleetServer)
 fleetServer.listen(4500, () => {
-    console.log("fleetServer started at 5500")
+    console.log("fleetServer started at 4500")
 })
 
 // print a message when request is received
@@ -32,6 +77,9 @@ app.use((req, res, next) => {
     console.log(req.path, req.method, "got request")
     next()
 })
+
+// swagger documenting
+app.use("/api-docs", serve, setup(combinedSwaggerSpec));
 
 // routing
 app.use('/api/fleet', fleetRoutes)
