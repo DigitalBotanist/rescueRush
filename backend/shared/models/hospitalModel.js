fbd441f0152faa934d803194b544812c4ff05a2c
Inushki <itlab3378@gmail.com>
2025-03-20 17:37:17 +0530

    create hospital.js,hospitalController.js and hospitalModel.js
    Not change the package.json file. Not create the frontend.

--- backend/shared/models/hospitalModel.js
+++ backend/shared/models/hospitalModel.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose"
 
+
 const Schema = mongoose.Schema
 
 const hospitalSchema = new Schema ({
@@ -16,7 +17,42 @@ const hospitalSchema = new Schema ({
     name: {
         type: String, 
         required: true
+    },
+    Bed:{
+        type:Number,
+        require:true,
+        default: 0
+    },
+    special_facilities: {
+        type:Boolean,
+        require:true,
+        default: false
     }
-})
+},{timestamps:true})
 
-export default model("Hospital", hospitalSchema)
+
+hospitalSchema.statics.createNew = async function(location,name,Bed,special_facilities){
+    // validation
+    if(!location|| !name|| !Bed|| !special_facilities){
+        throw Error ('All field must be fill');
+    }
+
+    //Check the data type of bed
+    if(Number.isInteger(Bed)&& Bed>0){
+        console.log("Valid data type")
+    }else{
+        console.log("Invalid data type")
+    }
+
+    //check the data type of special_facilities
+    if (typeof special_facilities === 'boolean') {
+        console.log("Valid data type");
+    } else {
+        console.log("Invalid data type");
+    }
+   
+    const hospital = await this.create({location,name,Bed,special_facilities})
+    return hospital
+}
+
+export default mongoose.model("Hospital", hospitalSchema)
