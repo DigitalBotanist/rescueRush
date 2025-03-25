d4113dc709aa9879e21b0a1d137da3072760308b
ITPy2s2 <itproject145@gmail.com>
2025-03-25 16:42:37 +0530

    delete user.js

--- backend/shared/routes/user.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import userController from '../controllers/userController.js';
-
-const router = Router();
-
-router.post('/login', userController.login);
-
-export default router;
