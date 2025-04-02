d45eb23bc2fa39d6a68788e992b99cb5edc739d7
ITPy2s2 <itproject145@gmail.com>
2025-04-02 23:39:10 +0530

    resourse frontend

--- frontend/src/components/AddNewResource.jsx
+++ frontend/src/components/AddNewResource.jsx
@@ -1,79 +1,3 @@
-<<<<<<< HEAD
-import React, { useEffect, useState } from "react";
-import "../css/resources.css";
-import { useAuthContext } from "../hooks/useAuthContext";
-
-function Medicalresources() {
-    const { user } = useAuthContext();
-    const [resources, setResources] = useState(null);
-
-    // Fetch existing schedules
-    useEffect(() => {
-        const fetchResources = async () => {
-            try {
-                const response = await fetch("api/resources/resourse", {
-                    headers: {
-                        "Content-Type": "application/json",
-                        Authorization: `Bearer ${user.token}`,
-                    },
-                });
-                const data = await response.json();
-                setResources(data);
-                console.log(data);
-            } catch (error) {
-                console.error("Error fetching schedules:", error);
-            }
-        };
-        fetchResources();
-    }, [user.token]); // Adding user.token as a dependency
-
-    return (
-        <div className="ra-container">
-            <div className="ra-content">
-                <main className="ra-main">
-                    {/* Title row n edt btn*/}
-                    <div className="ra-header-row">
-                        <h1><center>Medical Recourse Allocation</center></h1>
-                        <button className="ra-add-btn">Add</button>
-                    </div>
-
-                    {/* Table*/}
-                    <table className="ra-table">
-                        <thead>
-                            <tr>
-                                <th>Medical Id</th>
-                                <th>Name</th>
-                                <th>Quantity</th>
-                                <th>Allocated Amount</th>
-                                <th>Remaining Amount</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {resources && resources.length > 0 ? (
-                                resources.map((resource) => (
-                                    <tr key={resource.medID}>
-                                      <td>{resource.medID}</td>
-                                        <td>{resource.medName}</td>
-                                        <td>{resource.quantity}</td>
-                                        <td>{resource.allocatedAmount}</td>
-                                        <td>{resource.remainingAmount}</td>
-                                    </tr>
-                                ))
-                            ) : (
-                                <tr>
-                                    <td colSpan="6">No resources available.</td>
-                                </tr>
-                            )}
-                        </tbody>
-                    </table>
-
-                    <div className="ra-footer-row">
-                        <button className="ra-edit-btn">Edit</button>
-                        <button className="ra-edit-btn">
-                            Generate report
-                        </button>
-                    </div>
-=======
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "../css/resources.css";
@@ -238,17 +162,11 @@ const AddNewResource = () => {
                             ))}
                         </tbody>
                     </table>
->>>>>>> 5042ed577dfdfd951ab55948d953db2c2d7912d5
                 </main>
             </div>
         </div>
     );
-<<<<<<< HEAD
-}
 
-export default Medicalresources;
-=======
 };
 
 export default AddNewResource;
->>>>>>> 5042ed577dfdfd951ab55948d953db2c2d7912d5
