b13b95caf80a9437fda2a44074083fc9f6830545
Inushki <itlab3378@gmail.com>
2025-05-01 10:21:57 +0530

    delete package_lock.jsson and package.json

--- frontend/src/components/HospitalReport.jsx
+++ frontend/src/components/HospitalReport.jsx
@@ -11,7 +11,7 @@ function HospitalReport() {
     
   return (
     <div>
-        <div className='flex flex-row'>
+        <div className='flex flex-row '>
                         <div className='h-screen bg-[#EC221F] p-10 w-80 flex flex-col space-y-8 '>
                             <h1 className='text-white text-2xl ' >{user.firstName} {user.lastName}</h1>
                             <Link to ="/hospital/HospitalStaffDashBoard"> <h3 className='text-white hover:bg-[#B61E1E] hover:rounded-xl p-3'>Doctor Details</h3></Link>
